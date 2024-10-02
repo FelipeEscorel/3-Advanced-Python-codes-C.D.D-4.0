4919c3f680e2e00e859808cc7fe6d8108d50e1b9
Felipe Escorel <felipeescorel@hotmail.com>
2024-10-02 11:01:08 -0300

    Update testeFuncao2.py

--- testeFuncao2.py
+++ testeFuncao2.py
@@ -1,7 +1,5 @@
-def piramide(num):
-    for a in range (1,num+1):
-        for b in range (1, a+1):
-            print(a, end = " ")
-        print()
+from biblioteca import piramide, piramide2
 num = int(input("Digite um número: "))
 piramide(num)
+num2 = int(input("\nDigite um número: "))
+piramide2(num2)
