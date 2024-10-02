3e876cad5a7f52d0284ee4a84bd9156a735ff9a5
Felipe Escorel <felipeescorel@hotmail.com>
2024-10-02 11:36:07 -0300

    Update biblioteca.py

--- biblioteca.py
+++ biblioteca.py
@@ -3,9 +3,14 @@ def piramide(num):
         for b in range (1, a+1):
             print(a, end = " ")
         print()
-
 def piramide2(num):
     for a in range(1, num+1):
         for b in range(1, a+1):
             print(b, end = " ")
         print()
+def vogais(num):
+    cont = 0
+    for a in num:
+        if a in "aeiouAEIOU":
+            cont += 1
+    print(f"O texto digitado possui {cont} vogais")
